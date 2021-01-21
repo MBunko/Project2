ad7311535cc4bae96443b3182a42b1604b59c96e
Michael Bunko <73492032+MBunko@users.noreply.github.com>
2021-01-21 14:45:19 +0000

    Update test.sh

--- jenkinsbash/test.sh
+++ jenkinsbash/test.sh
@@ -8,17 +8,17 @@ python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
 python3 -m pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml 
-cd..
+cd ..
 
-#cd service2
-#python3 -m pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
-#cd..
+cd service2
+python3 -m pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
+cd ..
 
-#cd service3
-#python3 -m pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
-#cd..
+cd service3
+python3 -m pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
+cd ..
 
-#cd service4
-#python3 -m pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
-#deactivate 
-#cd..
+cd service4
+python3 -m pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
+deactivate 
+cd ..
