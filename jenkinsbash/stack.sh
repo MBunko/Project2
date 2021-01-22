d5ba903d00cebebf10de30efc0c9a6c091de6912
Michael Bunko <73492032+MBunko@users.noreply.github.com>
2021-01-22 15:59:27 +0000

    Update stack.sh

--- jenkinsbash/stack.sh
+++ jenkinsbash/stack.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 scp -i ~/.ssh/id_rsa docker-compose.yaml jenkins@projectm:/home/jenkins/docker-compose.yaml
 ssh -i ~/.ssh/id_rsa jenkins@projectm << EOF
-    export DATABASE_URI=${DATABASE_URI}
+    export DATABASE_URI=${DATABASE_URI} app_version=${app_version}
     docker stack deploy --compose-file /home/jenkins/docker-compose.yaml roller
 EOF
