3e71fcd166679ee7d2ae509802583e408c9a185e
Michael Bunko <73492032+MBunko@users.noreply.github.com>
2021-01-25 00:50:28 +0000

    Feature randroll from past
    * Title update
    
    * v3
    
    * Update index.html
    
    * Update Jenkinsfile
    
    * Update routes.py
    
    * Update index.html

--- Jenkinsfile
+++ Jenkinsfile
@@ -3,8 +3,8 @@ pipeline{
     environment{
         DATABASE_URI = credentials("DATABASE_URI")
         DATABASE_URI2= credentials("DATABASE_URI2")
-        app_version=1
-        rollback='true'
+        app_version=3
+        rollback='false'
     }
     stages{
         stage("Tests"){
--- service1/application/routes.py
+++ service1/application/routes.py
@@ -2,6 +2,7 @@ from application import app, db
 from application.models import Character
 from flask import request, render_template, jsonify
 import requests
+import random
 from sqlalchemy import desc
 
 @app.route('/')
@@ -10,9 +11,12 @@ def index():
     race1 = requests.get("http://roller_service3:5000/race")
     adjective=requests.get("http://roller_service4:5000/adjective", json={"class2":class1.text, "race2":race1.text})
     char=Character.query.order_by(desc("Id")).limit(5).all()
+    maxno=Character.query.order_by(desc("Id")).first()
+    randno=random.randint(1,maxno.Id)
+    randchar=Character.query.filter_by(Id=randno).all()
     new_character= Character(Character_class=class1.text, Character_race=race1.text, Character_description=adjective.text)
     db.session.add(new_character)
     db.session.commit()
     
-    return render_template("index.html", class0=class1.text, race=race1.text, adj=adjective.text, char=char)
+    return render_template("index.html", class0=class1.text, race=race1.text, adj=adjective.text, char=char, randchar=randchar)
 
